1f275613783a19de182fb261e9a32ec7cbca125e
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-12 17:44:56 +0300

    refactor file LoginTest

--- src/test/java/by/oz/LoginTest.java
+++ src/test/java/by/oz/LoginTest.java
@@ -26,12 +26,12 @@ public class LoginTest {
                 .then()
                 .log().all()
                 .statusCode(422)
-                .body("message", equalTo("Введите корректный номер мобильного телефона"));
+                .body("error", equalTo("Введите корректный номер мобильного телефона"));
     }
 
     @Test
-    @DisplayName("Test")
-    public void testLoginWithEmptyPhone(){
+    @DisplayName("Test login with empty phone")
+    public void testLoginWithEmptyPhone() {
         String phone = "";
 
         given()
@@ -42,6 +42,54 @@ public class LoginTest {
                 .then()
                 .log().all()
                 .statusCode(422)
-                .body("message", equalTo("Введите номер мобильного телефона белорусских операторов"));
+                .body("error", equalTo("Введите номер мобильного телефона белорусских операторов"));
+    }
+
+    @Test
+    @DisplayName("Test login with incorrect login length")
+    public void testLoginWithIncorrectPhoneLength() {
+        String phone = "37529756";
+
+        given()
+                .spec(LoginRequest.requestSpecification)
+                .body(LoginRequest.getBody(phone))
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .statusCode(422)
+                .body("error", equalTo("Введите номер мобильного телефона белорусских операторов"));
+    }
+
+    @Test
+    @DisplayName("Test login with incorrect login with symbol")
+    public void testLoginWithIncorrectPhoneWithSymbol() {
+        String phone = "37529756#*02";
+
+        given()
+                .spec(LoginRequest.requestSpecification)
+                .body(LoginRequest.getBody(phone))
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .statusCode(422)
+                .body("error", equalTo("Введите номер мобильного телефона белорусских операторов"));
+    }
+
+    @Test
+    @DisplayName("Test login with empty body")
+    public void testLoginWithEmptyBody() {
+        String body= "";
+
+        given()
+                .spec(LoginRequest.requestSpecification)
+                .body(body)
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .statusCode(500)
+                .body("error", equalTo("Произошла ошибка на сервисе авторизации."));
     }
 }
